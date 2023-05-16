65f1c01753279bfaf654b3e2fe440362c9c963dd
amirhossein-mlkz <amirhossein.mlkz@gmail.com>
2023-05-16 21:52:01 +0430

    examples changed

--- examples/cameraEmulation.py
+++ examples/cameraEmulation.py
@@ -2,14 +2,14 @@ import dorsaPylon
 from dorsaPylon import Collector, Camera
 
 
-if __name__ == "__main__":
-    collector = Collector()
-    #enable camera emulation and pass your ideal camera counts
-    collector.enable_camera_emulation(2)
-    #get avialble cameras in class of emlulation
-    cameras = collector.get_all_cameras(camera_class=dorsaPylon.CamersClass.emulation)
-    cam1 = cameras[0]
-    #-----------------------------------------------------------------
-    cam1.Parms.set_gain(50)
-    cam1.Operations.start_grabbing()
+
+collector = Collector()
+#enable camera emulation and pass your ideal camera counts
+collector.enable_camera_emulation(2)
+#get avialble cameras in class of emlulation
+cameras = collector.get_all_cameras(camera_class=dorsaPylon.CamersClass.emulation)
+cam1 = cameras[0]
+#-----------------------------------------------------------------
+cam1.Parms.set_gain(50)
+cam1.Operations.start_grabbing()
     
--- examples/getCameras.py
+++ examples/getCameras.py
@@ -2,10 +2,15 @@ import dorsaPylon
 from dorsaPylon import Collector, Camera
 
 
-if __name__ == "__main__":
-    collector = Collector()
-    #get avialble cameras That Are GigE
-    gige_cameras = collector.get_all_cameras(camera_class=dorsaPylon.CamersClass.gige)
-    #-----------------------------------------------------------------
-    #get all avialble cameras 
-    all_cameras = collector.get_all_cameras(camera_class=None)
+collector = Collector()
+
+#get avialble cameras That Are GigE
+gige_cameras = collector.get_all_cameras(camera_class=dorsaPylon.CamersClass.gige)
+
+#-----------------------------------------------------------------
+#get all avialble cameras 
+all_cameras = collector.get_all_cameras(camera_class=None)
+
+#-----------------------------------------------------------------
+#get specific camera
+cam = collector.get_camera_by_serial('123456')
